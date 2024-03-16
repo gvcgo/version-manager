999f59d38ace54e11914857d7349412e9d2ff2ee
moqsien <moqsien@foxmail.com>
2024-03-16 17:50:04 +0800

    symbolic for windows

--- pkgs/utils/symlink.go
+++ pkgs/utils/symlink.go
@@ -32,8 +32,9 @@ func SymbolicLink(oldname, newname string) error {
 	if err != nil {
 		gprint.PrintError("create symbolic failed: %+v\n", err)
 		if runtime.GOOS == gutils.Windows {
-			gprint.PrintWarning("If you're on Windows11, then go to 'System>For developers' and enable the 'Developer Mode'.")
+			gprint.PrintWarning("If you're on Windows11, then go to 'System>For developers>' and enable the 'Developer Mode'.")
 			gprint.PrintWarning("If you're on Windows10, then you can use vm with 'Admin Privilege'.")
+			gprint.PrintInfo("Note that, FAT32 and exFAT do not support symbolics. So you should not use vm with these partitions.")
 		}
 		os.Exit(1)
 	}
