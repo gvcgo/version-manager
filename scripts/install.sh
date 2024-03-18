e38e1144ca5dbcc23fff8a94911753be919c6583
moqsien <moqsien@foxmail.com>
2024-03-18 14:46:00 +0800

    script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
+# ====================================================
+# Installs/Updates version-manager(vm) for MacOS/Linux
+# ====================================================
+# Copyright (c) 2024 moqsien@hotmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 say() {
     printf 'rustup: %s\n' "$1"
 }
@@ -42,8 +64,12 @@ main() {
     need_cmd "unzip"
     need_cmd "mkdir"
 
+    echo "Downloading files..."
+
     curl -o "$filename" "$url"
 
+    ehco "Installing..."
+    
     if [ -s "./$filename" ]; then
         unzip "./$filename"
         chmod +x "./vm"
