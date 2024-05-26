eac11a05bb1a24c7e6848775051dcbbcfb3f1230
moqsien <moqsien@foxmail.com>
2024-05-26 12:33:24 +0800

    update

--- internal/installer/install/unarchiver.go
+++ internal/installer/install/unarchiver.go
@@ -1,6 +1,50 @@
 package install
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gvcgo/version-manager/internal/cnf"
+	"github.com/gvcgo/version-manager/internal/download"
+)
+
 /*
 Install use unarchiver.
 */
-type ArchiverInstaller struct{}
+type ArchiverInstaller struct {
+	OriginSDKName string
+	SDKName       string
+	VersionName   string
+	Version       download.Item
+}
+
+func NewArchiverInstaller() (a *ArchiverInstaller) {
+	a = &ArchiverInstaller{}
+	return
+}
+
+func (a *ArchiverInstaller) FormatSDKName() {
+	a.SDKName = a.OriginSDKName
+}
+
+func (a *ArchiverInstaller) GetInstallDir() string {
+	versionDir := cnf.GetVersionsDir()
+	d := filepath.Join(versionDir, a.SDKName)
+	os.MkdirAll(d, os.ModePerm)
+	return filepath.Join(d, fmt.Sprintf(VersionInstallDirPattern, a.OriginSDKName, a.VersionName))
+}
+
+func (a *ArchiverInstaller) GetSymbolLinkPath() string {
+	versionDir := cnf.GetVersionsDir()
+	d := filepath.Join(versionDir, a.SDKName)
+	os.MkdirAll(d, os.ModePerm)
+	return filepath.Join(d, a.SDKName)
+}
+
+func (a *ArchiverInstaller) Install(originSDKName, versionName string, version download.Item) {
+	a.OriginSDKName = originSDKName
+	a.VersionName = versionName
+	a.Version = version
+	a.FormatSDKName()
+}
--- internal/installer/installer.go
+++ internal/installer/installer.go
@@ -1,11 +1,61 @@
 package installer
 
+import (
+	"github.com/gvcgo/goutils/pkgs/gutils"
+	"github.com/gvcgo/version-manager/internal/download"
+	"github.com/gvcgo/version-manager/internal/installer/install"
+)
+
+func IsMinicondaInstalled() bool {
+	_, err := gutils.ExecuteSysCommand(true, "", "conda", "--help")
+	return err == nil
+}
+
+func IsCoursierInstalled() bool {
+	_, err := gutils.ExecuteSysCommand(true, "", "cs", "--help")
+	return err == nil
+}
+
+type SDKInstaller interface {
+	FormatSDKName()
+	GetInstallDir() string
+	GetSymbolLinkPath() string
+	Install(originSDKName, versionName string, version download.Item)
+}
+
 /*
 SDK Installer.
 */
-type Installer struct{}
+type Installer struct {
+	OriginSDKName string
+	VersionName   string
+	Version       download.Item
+	sdkInstaller  SDKInstaller
+}
 
-func NewInstaller() (i *Installer) {
-	i = &Installer{}
+func NewInstaller(originSDKName, versionName string, version download.Item) (i *Installer) {
+	i = &Installer{
+		OriginSDKName: originSDKName,
+		VersionName:   versionName,
+		Version:       version,
+	}
+	switch version.Installer {
+	case download.Conda, download.CondaForge:
+		i.sdkInstaller = install.NewCondaInstaller()
+	case download.Coursier:
+		i.sdkInstaller = install.NewCoursierInstaller()
+	case download.Executable, download.Dpkg, download.Rpm:
+		i.sdkInstaller = install.NewExeInstaller()
+	default:
+		i.sdkInstaller = install.NewArchiverInstaller()
+	}
 	return
 }
+
+func (i *Installer) Install() {}
+
+func (i *Installer) Uninstall() {}
+
+func (i *Installer) SetEnv() {}
+
+func (i *Installer) UnsetEnv() {}
