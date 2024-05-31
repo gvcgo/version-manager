f3655d408dc666c8a65fa2cee2e13b3834d8d874
moqsien <moqsien22@gmail.com>
2024-05-31 10:15:16 +0800

    zig

---
+++ internal/installer/post/zig.go
@@ -0,0 +1,32 @@
+package post
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+
+	"github.com/gvcgo/goutils/pkgs/gutils"
+	"github.com/gvcgo/version-manager/internal/cnf"
+	"github.com/gvcgo/version-manager/internal/download"
+)
+
+func init() {
+	RegisterPostInstallHandler(ZigSdkName, PostInstallForZig)
+}
+
+const (
+	ZigSdkName string = "zig"
+)
+
+/*
+post-installation handler for Bun.
+*/
+func PostInstallForZig(versionName string, version download.Item) {
+	versionDir := cnf.GetVersionsDir()
+	d := filepath.Join(versionDir, fmt.Sprintf("%s_versions", ZigSdkName))
+	zigInstallDir := filepath.Join(d, fmt.Sprintf("%s-%s", ZigSdkName, versionName))
+	if runtime.GOOS == gutils.Windows {
+		binPath := filepath.Join(zigInstallDir, "zig")
+		gutils.ExecuteSysCommand(true, zigInstallDir, "chmod", "+x", binPath)
+	}
+}
