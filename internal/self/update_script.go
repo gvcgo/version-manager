a5b1d586bce95a21dd06e26d86d48f2d5dd24c7f
moqsien <moqsien22@gmail.com>
2024-06-03 10:13:24 +0800

    update script

--- internal/self/update_script.go
+++ internal/self/update_script.go
@@ -18,10 +18,10 @@ const (
 )
 
 // var WinScript string = `powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://gvc.1710717.xyz/proxy/https://raw.githubusercontent.com/gvcgo/version-manager/main/scripts/install.ps1')"`
-var WinScript string = `powershell -c "irm https://gvc.1710717.xyz/proxy/https://raw.githubusercontent.com/gvcgo/version-manager/main/scripts/install.ps1 | iex"`
+var WinScript string = `powershell -c "irm https://gvc.1710717.xyz/proxy/https://raw.githubusercontent.com/gvcgo/version-manager/main/scripts/install.preview.ps1 | iex"`
 
 var UnixScript string = `#!/bin/sh
-curl --proto '=https' --tlsv1.2 -sSf https://gvc.1710717.xyz/proxy/https://raw.githubusercontent.com/gvcgo/version-manager/main/scripts/install.sh | sh`
+curl --proto '=https' --tlsv1.2 -sSf https://gvc.1710717.xyz/proxy/https://raw.githubusercontent.com/gvcgo/version-manager/main/scripts/install.preview.sh | sh`
 
 func setUpdateForWindows() {
 	scriptPath := filepath.Join(cnf.GetVMRWorkDir(), fmt.Sprintf("%s.bat", updateScriptName))
