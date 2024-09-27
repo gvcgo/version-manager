439b57fe6dc1ce1153554ab5bf8345c6af8e50dc
moqsien <moqsien@foxmail.com>
2024-09-27 17:51:34 +0800

    use cli

--- cmd/vmr/cli/hook.go
+++ cmd/vmr/cli/hook.go
@@ -1,7 +1,10 @@
 package cli
 
 import (
+	"strings"
+
 	"github.com/gvcgo/version-manager/cmd/vmr/cli/vcli"
+	"github.com/gvcgo/version-manager/internal/download"
 	"github.com/gvcgo/version-manager/internal/installer"
 	"github.com/spf13/cobra"
 )
@@ -13,8 +16,8 @@ var useHookCmd = &cobra.Command{
 	Use:     "use",
 	Aliases: []string{"u", "h"},
 	GroupID: vcli.GroupID,
-	Short:   "This is the hook for command cd.",
-	Long:    "Users do not use this command directly.",
+	Short:   "Installs and switches to a version for an SDK.",
+	Long:    "Example: vmr use sdkname@version.",
 	Run: func(cmd *cobra.Command, args []string) {
 		// enable locked version.
 		if ok, _ := cmd.Flags().GetBool("enable-locked-version"); ok {
@@ -22,14 +25,55 @@ var useHookCmd = &cobra.Command{
 			l.HookForCdCommand()
 		}
 
-		// use a version.
+		if len(args) == 0 {
+			cmd.Help()
+			return
+		}
 
-		// lock a version.
+		verInfo := args[0]
+		if !strings.Contains(verInfo, "@") {
+			cmd.Help()
+			return
+		}
 
-		// use a version only for current session.
+		vl := strings.Split(verInfo, "@")
+		if len(vl) != 2 {
+			cmd.Help()
+			return
+		}
+
+		sdkName := vl[0]
+		versionName := vl[1]
+
+		vList := download.GetVersionList(sdkName, "")
+		if len(vList) == 0 {
+			return
+		}
+
+		vItem, ok := vList[versionName]
+		if !ok {
+			return
+		}
+
+		ins := installer.NewInstaller(sdkName, versionName, "", vItem)
+
+		if ok, _ := cmd.Flags().GetBool("session-only"); ok {
+			// use a version only for current session.
+			ins.SetInvokeMode(installer.ModeSessionly)
+		} else if ok, _ := cmd.Flags().GetBool("lock-version"); ok {
+			// lock a version.
+			ins.SetInvokeMode(installer.ModeToLock)
+		} else {
+			// use a version globally.
+			ins.SetInvokeMode(installer.ModeGlobally)
+		}
+
+		ins.Install()
 	},
 }
 
 func init() {
 	useHookCmd.Flags().BoolP("enable-locked-version", "E", false, "To enable the locked version for current project.")
+	useHookCmd.Flags().BoolP("session-only", "s", false, "New a terminal session and add the specified version to the new session.")
+	useHookCmd.Flags().BoolP("lock-version", "l", false, "Lock the specific version for an SDK.")
 }
