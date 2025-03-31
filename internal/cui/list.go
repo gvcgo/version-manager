81b93b052526d61280b297ff06bf92f893238519
ericwei <moqsien@foxmail.com>
2025-03-31 18:05:23 +0800

    update

--- internal/cui/list.go
+++ internal/cui/list.go
@@ -11,16 +11,22 @@ type ListView struct {
 	keybindings    []KeyBinding
 	show           bool
 	data           []string
+	rawData        []string
 	selected       int
 	g              *gocui.Gui
 	x0, y0, x1, y1 int
+	searcher       *SearcherView
+	parent         *gocui.View
 }
 
 func NewList(viewName string, g *gocui.Gui) *ListView {
-	return &ListView{
+	l := &ListView{
 		viewName: viewName,
 		g:        g,
 	}
+
+	l.searcher = NewSearcher(l)
+	return l
 }
 
 func (l *ListView) SetCoord(x0, y0, x1, y1 int) {
@@ -30,6 +36,18 @@ func (l *ListView) SetCoord(x0, y0, x1, y1 int) {
 func (l *ListView) SetData(datalist ...string) {
 	l.data = datalist
 	l.selected = 0
+}
+
+func (l *ListView) SetRawData(datalist ...string) {
+	l.rawData = datalist
+	l.selected = 0
+	if l.data == nil {
+		l.data = datalist
+	}
+}
+
+func (l *ListView) Reset() {
+	l.data = l.rawData
 }
 
 func (l *ListView) Show(v *gocui.View) {
@@ -83,6 +101,54 @@ func (l *ListView) Up(g *gocui.Gui, v *gocui.View) error {
 	return nil
 }
 
-func (l *ListView) Search(g *gocui.Gui, v *gocui.View) error {
+func (l *ListView) On(g *gocui.Gui, parent *gocui.View) error {
+	l.parent = parent
+	if v, err := g.SetView(l.viewName, l.x0, l.y0, l.x1, l.y1, 0); err != nil {
+		if !gocui.IsUnknownView(err) {
+			return err
+		}
+		v.Title = l.viewName
+		v.FgColor = gocui.ColorCyan
+		v.Wrap = true
+		v.Autoscroll = true
+		l.Show(v)
+		_, err := SetViewOnTop(l.viewName, g)
+		if !gocui.IsUnknownView(err) {
+			return err
+		}
+	}
 	return nil
+}
+
+func (l *ListView) Off(g *gocui.Gui, _ *gocui.View) error {
+	if err := g.DeleteView(l.viewName); err != nil {
+		if !gocui.IsUnknownView(err) {
+			return err
+		}
+	}
+	_ = g.ForceLayoutAndRedraw()
+	if l.parent != nil {
+		if _, err := SetViewOnTop(l.parent.Name(), g); err != nil {
+			if !gocui.IsUnknownView(err) {
+				return err
+			}
+		}
+		l.parent = nil
+	}
+	return nil
+}
+
+func (l *ListView) Search(g *gocui.Gui, v *gocui.View) error {
+	if err := l.searcher.On(g, v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *ListView) InitKeys() {
+	if l.g != nil {
+		for _, kb := range l.keybindings {
+			_ = kb.Set(l.g, l.viewName)
+		}
+	}
 }
