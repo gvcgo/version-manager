bdd133abeb559b60f93c6436c6904391b9e60032
ericwei <moqsien@foxmail.com>
2025-06-10 13:28:58 +0800

    update

--- internal/cui/spinner_test.go
+++ internal/cui/spinner_test.go
@@ -1,23 +1,34 @@
 package cui
 
 import (
+	"fmt"
 	"testing"
 
 	_ "github.com/charmbracelet/bubbletea"
 )
 
 func TestSpinner(t *testing.T) {
-	// s := NewSpinner("test spinner")
+	s := NewSpinner("test spinner")
 
+	var h Hook = func() error {
+		fmt.Println("in hook")
+		return nil
+	}
+	s.SetCancelHook(h)
+
+	if s.cancel != nil {
+		err := s.cancel()
+		if err != nil {
+			t.Error(err)
+		}
+	}
 	// go func() {
 	// 	fmt.Println("stop spinning")
 	// 	s.Stop()
 	// }()
 
-	// go func() {
 	// 	if _, err := tea.NewProgram(s).Run(); err != nil {
 	// 		t.Error(err)
 	// 	}
-	// }()
 	// time.Sleep(time.Second)
 }
