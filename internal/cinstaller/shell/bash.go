9114e4f4bb4bbb0b3fd919a30daf5cf21abf26cd
ericwei <moqsien@foxmail.com>
2025-07-04 17:46:16 +0800

    envs

--- internal/cinstaller/shell/bash.go
+++ internal/cinstaller/shell/bash.go
@@ -1,1 +1,40 @@
 package shell
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gvcgo/version-manager/internal/cnf"
+)
+
+type Bash struct{}
+
+func NewBash() *Bash {
+	return &Bash{}
+}
+
+func (b *Bash) ConfPath() string {
+	homeDir, _ := os.UserHomeDir()
+	return filepath.Join(homeDir, ".bashrc")
+}
+
+func (b *Bash) VMREnvConfPath() string {
+	workDir := cnf.GetVMRWorkDir()
+	return filepath.Join(
+		workDir,
+		fmt.Sprintf("%s.sh", VMREnvFileName),
+	)
+}
+
+func (b *Bash) Init() {
+	// TODO: add cd hook, etc.
+}
+
+func (b *Bash) PackPath(pathS string) string {
+	return fmt.Sprintf("export PATH=%s:$PATH", pathS)
+}
+
+func (b *Bash) PackEnv(key, value string) string {
+	return fmt.Sprintf("export %s=%s", key, value)
+}
---
+++ internal/utils/common.go
@@ -0,0 +1,9 @@
+package utils
+
+const (
+	Windows string = "windows"
+	Linux   string = "linux"
+	MacOS   string = "darwin"
+	Arm64   string = "arm64"
+	Amd64   string = "amd64"
+)
